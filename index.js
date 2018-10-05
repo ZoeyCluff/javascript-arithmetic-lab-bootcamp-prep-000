7ac057ec0afb208592c23c0dde9ce5f6f803e947
Zoey Cluff <zoey.cluff@gmail.com>
2018-10-05 08:23:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function subtract(a, b) {
   var number = a - b
   return number
 }
+
+function multiply(a,b) {
+  var number = a * b
+  return number
+}
+
+function divide(a, b) {
+  var number = a / b 
+  return number
+}
+
+function inc(n) {
+  var number = a++
+  return number
+}
